a42ed52de28a4e636574155f70738f273d56f6e0
jj15warrior <janek302010@gmail.com>
2025-07-20 23:12:36 +0200

    overhauled the project and converted to newer python

--- blupi-scan.py
+++ blupi-scan.py
@@ -8,16 +8,17 @@ from os import devnull
 from collections import deque
 from math import sqrt
 
-# Global variables until we have a config file
-freqmin = 854000000 # DO NOT CHANGE
-freqmax = 860000000 # DO NOT CHANGE
+
+freqmin = 380000000 # european uplink
+freqmax = 385000000 # this should work for most for europe
+
 sensitivity = 50
 sysdamping = 10
 freqdamping = 100
 powerfftw_path = "/usr/local/bin/rtl_power_fftw"
-baseline_path = "baseline_data.dat"
+baseline_path = "./baseline_data.dat"
 totalbins = 960 * 3 # DO NOT CHANGE
-ppm_offset = 56
+ppm_offset = 7
 bline_int_t = 600 # Set to around 600 (10m)
 
 # Global that we'll keep
@@ -26,41 +27,37 @@ dvnll = open(devnull, 'wb')
 # Functions
 def average(p): return sum(p) / float(len(p))
 
-def variance(p): return map(lambda x: (x - average(p))**2, p)
+def variance(p): return [(x - average(p))**2 for x in p]
 
 def std_dev(p): return sqrt(average(variance(p)))
 
 def alert(p):
-
-	# Still developing...need to provide true alert functionality (GPIO, audio, etc)
 	freq = round(p[0] /1000000, 4)
-	print "At " + time.strftime("%H:%M:%S") + ", a " + str(round(p[1], 1)) + " dB/Hz signal was detected at " + str(freq) + " MHz."
+	print("At " + time.strftime("%H:%M:%S") + ", a " + str(round(p[1], 1)) + " dB/Hz signal was detected at " + str(freq) + " MHz.")
+	# here, you can add a custom alert like gpio buzzers, leds or displays
 
 def bline_build(fmin, fmax, bins, b_time, offset, bpath):
-	
 	t = "-t " + str(b_time)
 	array = []
 	spect = "-f " + str(fmin) + ":" + str(fmax)
-	b_bins = "-b " + str(bins/3)
+	b_bins = "-b " + str(int(bins/3))
 
 	base_gen = sp.Popen([powerfftw_path, spect, t, offset, b_bins], stdout=sp.PIPE, stderr=dvnll, shell=False)
-
+	print([powerfftw_path, spect, t, offset, b_bins])
 	for line in iter(base_gen.stdout.readline, b""):
 
-		with open(baseline_path, 'a') as baselinefile: baselinefile.write(line + '\n')
+		with open(baseline_path, 'a') as baselinefile:
+			baselinefile.write(str(line)[2:-3] + '\n')
 
-# Start your engines...
 if __name__ == '__main__':
-
-	# Import settings file here...
 
 	# Parameter formatting
 	spect = "-f " + str(freqmin) + ":" + str(freqmax)
 
-	fftb = totalbins / 3 # NEED TO DEVELOP
+	fftb = totalbins / 3
 
-	fftbins = "-b " + str(fftb)
-	ppm = "-p " + str(ppm_offset)
+	fftbins = "-b " + str(int(fftb))
+	ppm = "-p " + str(int(ppm_offset))
 	otherargs = "-c"
 	rolling = []
 	rolling_avg = deque([])
@@ -72,26 +69,25 @@ if __name__ == '__main__':
 
 	# Ready, set, GO!
 	try:
-		print "Starting up at", time.strftime("%H:%M:%S") + "..."
+		print("Starting up at", time.strftime("%H:%M:%S") + "...")
 		
 		# Check for baseline file
 		if not os.path.isfile(baseline_path):
 
-			print "I couldn't find a baseline file, so we're gonna generate one. This will take some time..."
+			print("Couldn't find a baseline file. Generating. This will take",bline_int_t/60,"min")
 
 			# Generate said baseline file
 			bline_build(freqmin, freqmax, fftb, bline_int_t, ppm, baseline_path)
-			print "Baseline file generation completed at", time.strftime("%H:%M:%S") + ". Starting up!"
+			print("Baseline file generation completed at", time.strftime("%H:%M:%S") + ". Starting up!")
 
 		rpf = sp.Popen([powerfftw_path, spect, otherargs, ppm, fftbins, baseline_file], stdout=sp.PIPE, stderr=dvnll, shell=False)
-
+		print([powerfftw_path, spect, otherargs, ppm, fftbins, baseline_file])
 		# Let's see what's going on with rtl_power_fftw
 		for line in iter(rpf.stdout.readline, b""):
-
 			# Ignore garbage output
-			if not ('#' in line or not line.strip()):
+			if not (bytes('#', "utf-8") in line or not line.strip()):
 
-				floats = map(float, line.split())
+				floats = list(map(float, line.split()))
 
 				# Create 2D array if it isn't already defined
 				if len(rolling) < totalbins: rolling.append(deque([]))
@@ -104,11 +100,8 @@ if __name__ == '__main__':
 					rolling[i].popleft()
 					alarmthresh = average(rolling[i]) + stddev / sensitivity * 25000
 
-					#if i == 0: print floats[1], alarmthresh - floats[1]
-
 					# There be coppers!
 					if floats[1] > alarmthresh:
-
 						alert(floats)
 
 				# Maintain sweep length at the total number of samples
@@ -128,6 +121,4 @@ if __name__ == '__main__':
 
 
 	except (KeyboardInterrupt, SystemExit): # Press ctrl-c
-
-		rpf.kill()
-		print "\n", "Buh-bye"
+		print("\n", "Buh-bye")
